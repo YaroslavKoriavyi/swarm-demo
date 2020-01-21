f982a0d261b301c6ac1fb294b49faf5cbd83e506
Yaroslav Koriavyi <yaroslav_koriavyi@epam.com>
2020-01-21 14:23:48 +0200

    Fix publish scripts, update ppt

--- publish.sh
+++ publish.sh
@@ -1,3 +1,1 @@
-sudo scp -r -i "swarm.pem" /mnt/d/Projects/swarm-demo/deploy ec2-user@3.94.184.64:~/v1
-
-sudo chmod +x filename.sh
+sudo scp -r -i "swarm.pem" /mnt/d/Projects/swarm-demo/deploy ec2-user@3.95.234.8:~/v1
